1c216c8a60da4350bb8ee9da7b3ec357c923c655
Palanikumar Palanichamy <67886600+palanikumarraaj@users.noreply.github.com>
2024-03-06 10:24:02 +0530

    Catch_ID-counter.R
    Catch_ID-counter.R - To catch and count the rsID in TSV files

---
+++ Catch_ID-counter.R
@@ -0,0 +1,23 @@
+library(dplyr)
+# Step 1: Collect input files
+tsv_files <- list.files(pattern = "\\.tsv$")
+args <- commandArgs(trailingOnly = TRUE) # accept input file and output file name from the user
+vcf_rawdata <- args[1] # push input file i.e. virulence excel sheet
+input_csv <- read.csv(vcf_rawdata, header = TRUE)
+input_csv[, tsv_files] <- NA
+##working
+# Step 2: Count matching patterns
+for (tsv_file in tsv_files) {
+  tsv_data <- read.delim(tsv_file, sep = "\t", header = TRUE)
+  for (i in 1:nrow(input_csv)) {
+    row <- input_csv[i, ]
+    match_index <- which(
+      tsv_data$ID == row$ID
+    )
+    input_csv[i, tsv_file] <- ifelse(length(match_index) > 0, 1, 0)
+  }
+    rm(tsv_data) # Remove TSV data to save memory
+}
+# Step 3: Write output CSV
+samplename <- gsub(".csv", "", vcf_rawdata)
+write.csv(input_csv, paste0(samplename,"_IDCount.csv"), row.names=FALSE)
