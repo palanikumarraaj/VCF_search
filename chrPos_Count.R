bc33f7252030fee4dddd7ac64fc30a16261b851d
Palanikumar Palanichamy <67886600+palanikumarraaj@users.noreply.github.com>
2024-03-06 10:08:27 +0530

    chrPos_Count.R
    ChrPos_counter - found the Chr Pos in VCF converted TSV files

---
+++ chrPos_Count.R
@@ -0,0 +1,24 @@
+library(dplyr)
+tsv_files <- list.files(pattern = "\\.tsv$")
+args <- commandArgs(trailingOnly = TRUE) # accept input file and output file name from the user
+vcf_rawdata <- args[1] # push input file i.e. virulence excel sheet
+input_csv <- read.csv(vcf_rawdata, header = TRUE)
+input_csv[, tsv_files] <- NA
+##working
+# Step 2: Count matching patterns
+for (tsv_file in tsv_files) {
+  tsv_data <- read.delim(tsv_file, sep = "\t", header = TRUE)
+  for (i in 1:nrow(input_csv)) {
+    row <- input_csv[i, ]
+    match_index <- which(
+      tsv_data$CHR == row$CHR &
+        tsv_data$POS == row$POS
+    )
+    input_csv[i, tsv_file] <- ifelse(length(match_index) > 0, 1, 0)
+  }
+  
+  rm(tsv_data) # Remove TSV data to save memory
+}
+# Step 3: Write output CSV
+samplename <- gsub(".csv", "", vcf_rawdata)
+write.csv(input_csv, paste0(samplename,"_ChrPosCount.csv"), row.names=FALSE)
